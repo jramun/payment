ee28c99d4ab49049bd1d5b3faf114c06c450a472
Mojtaba Sharif <mojtabasharif777@gmail.com>
2019-03-21 21:53:49 +0330

    edit

--- src/main/java/com/jramun/payment/PaymentApplication.java
+++ src/main/java/com/jramun/payment/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.jramun.payment.core;
+package com.jramun.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
